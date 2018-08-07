02626b96b8b73f10b1a0264bff54d4933c591f5d
Ulan Nurmatov <ulannurmatov@ulans-macbook-pro.local>
2018-08-07 19:49:21 +0600

    Network created

---
+++ CoursesApp/Network/ServerManager.swift
@@ -0,0 +1,32 @@
+//
+//  ServerManager.swift
+//  CoursesApp
+//
+//  Created by Ulan Nurmatov on 8/7/18.
+//  Copyright © 2018 Ulan Nurmatov. All rights reserved.
+//
+
+import Foundation
+
+class ServerManager: HTTPRequestManager {
+    static let shared = ServerManager()
+    
+    func getCities(completion: @escaping (CityResult) -> (), error: @escaping (String) -> ()) {
+        self.get(endpoint: Constants.Network.EndPoint.cities, completion: { (data) in
+            //TODO
+            do {
+                guard let  data = data else { return }
+                let result = try JSONDecoder().decode(CityResult.self, from: data)
+                completion(result)
+            }
+            catch let errorMessage {
+                error(errorMessage.localizedDescription)
+            }
+            
+        }) { (errorMessage) in
+            error(errorMessage)
+        }
+}
+    
+    
+}
