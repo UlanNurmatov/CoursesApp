e870c71c1b816bd074426a572bc9cb4ef843f406
Ulan Nurmatov <ulannurmatov@ulans-macbook-pro.local>
2018-08-18 02:35:26 +0600

    Facebook login and share pods installed

---
+++ CoursesApp/ViewControllers/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  CoursesApp
+//
+//  Created by Ulan Nurmatov on 8/18/18.
+//  Copyright © 2018 Ulan Nurmatov. All rights reserved.
+//
+
+import UIKit
+import FacebookLogin
+
+class LoginViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
